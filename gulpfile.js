e710c0e5a39e8fd15b20dc43db7082519a8ee7d9
Vinicius Cainelli <vicainelli@gmail.com>
2018-07-22 15:10:39 -0300

    Add build and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,25 @@ const sass = require("gulp-sass");
 const pug = require('gulp-pug');
 
 // STYLES
-gulp.task('sass', function () {
+gulp.task('styles', () => {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
 
 // VIEWS
-gulp.task('views', function buildHTML() {
+gulp.task('views', () => {
   return gulp.src('./src/views/**/*.pug')
   .pipe(plumber())
   .pipe(pug({}))
   .pipe(gulp.dest('./dist/'))
 });
+
+// WATCH
+gulp.task('watch', function () {
+  gulp.watch("./src/views/**/*.pug", ["views"]);
+  gulp.watch("./src/sass/*.scss", ["styles"]);
+});
+
+// BUILD
+gulp.task('build', ['views', 'styles']);
